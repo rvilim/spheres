a85c9ef3ef1787d157ac14d6b9a803500260b970
Ryan Vilim <rvilim@character.ai>
2025-01-30 10:54:55 -0500

    wip

---
+++ build_pybind.sh
@@ -0,0 +1,6 @@
+clang++ -O3 -Wall -shared -std=c++20 -fPIC -DWITH_PYTHON \
+    $(python3 -m pybind11 --includes) \
+    src/piles.cpp src/bitfiltertree.cpp \
+    -undefined dynamic_lookup \
+    -fopenmp \
+    -o piles$(python3-config --extension-suffix)
---
+++ build_tree.py
@@ -0,0 +1,3 @@
+import piles
+solver = piles.PileSolver()
+tree = solver.build_diophantine_tree(csv_path="diophantine_single.txt", tree_path='tree_single.bin')
